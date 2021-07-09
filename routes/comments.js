00085466638128c5fd51800298db35ba91392390
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-07-08 22:12:46 -0400

    created GET, POST,POST,DELETE endpoint: POSTMAN test successful

--- routes/comments.js
+++ routes/comments.js
@@ -4,6 +4,27 @@ const router = express.Router();
 
 
 // All endpoints and route handlers go here
+////////////////////////////////////////////////////////// GET //////////////////////////////////////////
+router.get('/', async (req, res) => {
+    try {
+    const comments = await Comment.find();
+    return res.send(comments);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+}});
+////////////////////////////////////////////////////////// GET By ID //////////////////////////////////////////
+router.get('/:id', async (req, res) => {
+    try {
+   
+    const comment = await Comment.findById(req.params.id);
+    if (!comment)
+    return res.status(400).send(`The comment with id "${req.params.id}" does not exist.`);
+    return res.send(comment);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+////////////////////////////////////////////////////////// POST //////////////////////////////////////////
  router.post('/', async (req, res) => {
     try {
         const { error } = validate(req.body);  
@@ -23,22 +44,42 @@ const router = express.Router();
     } catch (ex) {
     return res.status(500).send(`Internal Server Error: ${ex}`);
     }
-   }); 
-   
-/*
-router.post('/', async (req, res) => {
+}); 
+//////////////////////////////////////////////////////////////////// PUT ////////////////////////////////////////
+router.put('/:id', async (req, res) => {
     try {
-   
-    const comment = new Comment({
-    text: 'Stanley Classic Vacuum Bottle',
-    likes: 19.82, 
-    dislikes: 1,
-    });
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error);
+    const comment = await Comment.findByIdAndUpdate(
+    req.params.id,
+    {
+        text: req.body.text,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes, 
+        videoId: req.body.videoId,
+    },
+    { new: true }
+    );
+    if (!comment)
+    return res.status(400).send(`The comment with id "${req.params.id}" d
+   oes not exist.`);
     await comment.save();
     return res.send(comment);
     } catch (ex) {
     return res.status(500).send(`Internal Server Error: ${ex}`);
     }
-   });
-*/
+}); 
+//////////////////////////////////////////////////////////////////// DELETE ////////////////////////////////////////
+router.delete('/:id', async (req, res) => {
+    try {
+   
+    const comment = await Comment.findByIdAndRemove(req.params.id);
+    if (!comment)
+    return res.status(400).send(`The comment with id "${req.params.id}" does not exist.`);
+    return res.send(comment);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = router;
